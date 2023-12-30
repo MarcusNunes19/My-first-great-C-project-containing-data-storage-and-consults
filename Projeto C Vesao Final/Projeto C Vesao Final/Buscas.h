d99764e5f1ab9ce510921fefdcf372de860a5e7b
Marcus Vinicius Oliveira Nunes <134748180+MarcusNunes19@users.noreply.github.com>
2023-12-30 18:53:46 -0300

    Delete Projeto C Vesao Final directory

--- Projeto C Vesao Final/Projeto C Vesao Final/Buscas.h
+++
@@ -1,8 +0,0 @@
-#ifndef BUSCAS_H
-#define BUSCAS_H
-
-void reimprimirPassagem();
-void chamarListaCliente();
-void consultarCliente();
-
-#endif // BUSCAS_H
--- Projeto C Vesao Final/Projeto C Vesao Final/FunCad.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCAD_H
-#define FUNCAD_H
-
-void checar_carac(char *vet, int tam);
-void cadastrar_cliente();
-void passagem();
-
-#endif // FUNCAD_H
